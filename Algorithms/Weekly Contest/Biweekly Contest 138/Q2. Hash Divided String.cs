ebfcecf40fbf6956ee2520fb4a8e8073208cc732
Hikimory <nikita.bikon@gmail.com>
2024-08-31 19:22:11 +0300

    add biweekly contest solutions

---
+++ Algorithms/Weekly Contest/Biweekly Contest 138/Q2. Hash Divided String.cs
@@ -0,0 +1,56 @@
+// An integer n is strictly palindromic if, for every base b between 2 and n - 2 (inclusive), 
+// the string representation of the integer n in base b is palindromic.
+// Given an integer n, return true if n is strictly palindromic and false otherwise.
+// A string is palindromic if it reads the same forward and backward.
+
+// Example 1:
+// Input: n = 9
+// Output: false
+// Explanation: In base 2: 9 = 1001 (base 2), which is palindromic.
+// In base 3: 9 = 100 (base 3), which is not palindromic.
+// Therefore, 9 is not strictly palindromic so we return false.
+// Note that in bases 4, 5, 6, and 7, n = 9 is also not palindromic.
+
+// Example 2:
+// Input: n = 4
+// Output: false
+// Explanation: We only consider base 2: 4 = 100 (base 2), which is not palindromic.
+// Therefore, we return false.
+
+// Constraints:
+// 4 <= n <= 10^5
+
+public class Solution 
+{
+    public bool IsStrictlyPalindromic(int n) 
+    {
+        for(int i = 2; i <= n - 2; i ++)
+        {
+            string b = ConvertToBase(n, i);
+            
+            char[] array = b.ToCharArray();
+            Array.Reverse(array);
+            string part2 = new string(array);
+            
+            if(b != part2)
+                return false;
+        }
+        return true;
+    }
+    
+    public string ConvertToBase(int value, int toBase)
+    {
+        string symbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if (toBase < 2 || toBase > 36)
+        {
+            throw new ArgumentOutOfRangeException("toBase", "Base must be between 2 and 36");
+        }
+        string baseNumber = "";
+        while (value > 0)
+        {
+            baseNumber = symbols[(int)(value % toBase)] + baseNumber;
+            value /= toBase;
+        }
+        return baseNumber == "" ? "0" : baseNumber;
+    }
+}
